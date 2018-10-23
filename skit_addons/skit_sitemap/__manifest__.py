c8d726020881e3ebace3e67576b1a41f03257c3d
e_selvi <e_selvi@7d40962c-ea0b-6842-bdfc-460be783c1db>
2018-10-23 12:37:28 +0000

    [ODOCA-2]-Sitemap for Odoo 12

---
+++ skit_addons/skit_sitemap/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Website Sitemap',
+    'category': 'Website',
+    'summary': 'This module helps us to show sitemap link on your website',
+    'author': 'Srikesh Infotech',
+    'website': 'http://www.srikeshinfotech.com',
+    'version': '12.0',
+    'description': """This module helps us to show sitemap link on your website""",
+    'price': 13,
+    'currency': 'EUR',
+    'images': ['images/main_screenshot.png'],
+    'depends': [
+                'website',
+                ],
+    'data': [
+        "views/sitemap_templates.xml",
+    ],
+    'installable': True,
+    'application': True,
+}
