17d1d5d770fc88ac97ba660e19d5e2fa238b769c
narmadha <narmadha@7d40962c-ea0b-6842-bdfc-460be783c1db>
2018-10-29 07:20:44 +0000

    [ODOCA-4] - Freeze Header for Odoo12.

---
+++ skit_addons/skit_freeze_header/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Freeze Header',
+    'category': 'Website',
+    'author': 'Srikesh Infotech',
+    'website': 'http://www.srikeshinfotech.com',
+    'version': '12.0',
+    'description': """This module helps us to freeze header while scroll""",
+    'depends': [
+                'web',
+                ],
+    'data': [
+        "views/freeze_head.xml",
+    ],
+    'installable': True,
+    'application': True,
+}
