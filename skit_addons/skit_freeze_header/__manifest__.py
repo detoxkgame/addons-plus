1520626ccfd7b2c5589ae5674eb04d8f3dfd5dab
narmadha <narmadha@7d40962c-ea0b-6842-bdfc-460be783c1db>
2018-10-30 04:30:52 +0000

    [ODOCA-4] - Freeze Header for Odoo12. Changed Manifest File and Included Images Folder.

--- skit_addons/skit_freeze_header/__manifest__.py
+++ skit_addons/skit_freeze_header/__manifest__.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'Freeze Header',
-    'category': 'Website',
-    'author': 'Srikesh Infotech',
-    'website': 'http://www.srikeshinfotech.com',
-    'version': '12.0',
-    'description': """This module helps us to freeze header while scroll""",
-    'depends': [
-                'web',
-                ],
+    'name': "Freeze List View Header",
+
+    'summary': """
+        To freeze list view's & grid view's header, very helpful when dealing with many record.
+    """,
+
+    'description': """
+        To freeze list view's & grid view's header, very helpful when dealing with many record.
+    """,
+    'price': 25,
+    'currency': 'EUR',
+    'author': "Srikesh Infotech",
+    'license': "AGPL-3",
+    'website': "www.srikeshinfotech.com",
+    'category': 'web',
+    'version': '1.0',
+    'images': ['images/main_screenshot.png'],
+    'depends': ['web','base'],
     'data': [
         "views/freeze_head.xml",
     ],
     'installable': True,
+    'auto_install': False,
     'application': True,
 }
