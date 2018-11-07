15b0d5720b20616fac019680745e4cd49624bdb6
dhanam <dhanam@7d40962c-ea0b-6842-bdfc-460be783c1db>
2018-11-07 07:53:13 +0000

    [OD12-018] - Financial Reports in Odoo 12

---
+++ skit_addons/skit_account_reports/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo12 Financial Reports in PDF',
+    'version': '1.1',
+    'summary': 'This module helps to generate Financial reports Balance Sheet, Profit and Loss, Trail Balance, General Ledger and Aged Partner Balance in PDF format',
+    'author': 'Srikesh Infotech',
+    'license': "AGPL-3",
+    'website': 'http://www.srikeshinfotech.com',
+    'price': 149,
+    'currency': 'EUR',
+    'description': """
+        This module helps to generate Financial reports Balance Sheet, Profit and Loss, Trail Balance, General Ledger and Aged Partner Balance in PDF format
+        """,
+    'images': ['images/main_screenshot.png'],
+    'category': "Invoicing Management",
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_financial_report_data.xml',
+        'views/account_menuitem.xml',
+        'views/account_view.xml',
+        'views/account_report.xml',
+        'wizard/account_report_trial_balance_view.xml',        
+        'views/report_trialbalance.xml',
+        'wizard/account_report_general_ledger_view.xml',
+        'views/report_generalledger.xml',
+        'wizard/account_financial_report_view.xml',
+        'views/report_financial.xml',
+        'wizard/account_report_aged_partner_balance_view.xml',        
+        'views/report_agedpartnerbalance.xml',
+        
+    ],
+    'installable': True,    
+    'auto_install': False,
+    'application': True,
+}
---
+++ skit_addons/skit_account_reports/report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import account_financial_report
+from . import account_report_financial
+from . import account_balance
+from . import account_general_ledger
+from . import account_aged_partner_balance
