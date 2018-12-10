50cdc0156484a40e191a8eeccc98ada44e9c1df7
vidhya <vidhya@7d40962c-ea0b-6842-bdfc-460be783c1db>
2018-12-10 04:17:29 +0000

    [OD12-566] - Hotel Management - Created the master, transaction table and form design

---
+++ skit_addons/skit_hotel_management/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hotel Management',
+    'version': '1.1',
+    'summary': 'Hotel Management',
+    'author': 'Srikesh Infotech',
+    'license': "AGPL-3",
+    'website': 'http://www.srikeshinfotech.com',
+    'price': 100,
+    'currency': 'EUR',
+    'description': """
+        Hotel Management
+        """,
+    #'images': ['images/main_screenshot.png'],
+    'category': "Hotel Management",
+    'depends': ['sale', 'purchase', 'point_of_sale'],
+    'data': [
+            'security/hotel_security.xml',
+            'security/ir.model.access.csv',
+            'views/hotel_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
