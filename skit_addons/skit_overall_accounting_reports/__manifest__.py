ac758b8baa392f87538497e5a66cab1e503b6703
kalpana <kalpana@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-01-21 12:00:38 +0000

    [OD12-1232]-Odoo12 Overall Accounting Reports

---
+++ skit_addons/skit_overall_accounting_reports/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Skit Overall Accounting Reports',
+    'version': '1.0',
+    'author': 'Srikesh Infotech',
+    'description': """
+            This module helps to view Overall Accounting Reports
+    """,
+    'category': 'Skit Overall Accounting Reports',
+    'website': 'www.srikeshinfotech.com',
+    'images': ['images/main_screenshot.png'],
+    'depends': ['base', 'account','skit_financial_form','skit_account_reports','skit_asset_management','skit_bank_reconcil'],
+   
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
