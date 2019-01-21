7053384d8c88a0992044930e254572d3801af2b5
dhanam <dhanam@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-01-21 12:08:31 +0000

    Renamed module name and added price

--- skit_addons/skit_overall_accounting_reports/__manifest__.py
+++ skit_addons/skit_overall_accounting_reports/__manifest__.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Skit Overall Accounting Reports',
+    'name': 'Odoo12 All-in-one Accounting reports',
     'version': '1.0',
     'author': 'Srikesh Infotech',
+    'license': "AGPL-3",
+    'website': 'http://www.srikeshinfotech.com',
+    'price': 125,
+    'currency': 'EUR',
     'description': """
-            This module helps to view Overall Accounting Reports
+            This module helps to generate all Accounting Reports in PDF, Assets Managements and Bank Reconciliation report
     """,
-    'category': 'Skit Overall Accounting Reports',
-    'website': 'www.srikeshinfotech.com',
+    'category': 'Accounting',
     'images': ['images/main_screenshot.png'],
     'depends': ['base', 'account','skit_financial_form','skit_account_reports','skit_asset_management','skit_bank_reconcil'],
    
