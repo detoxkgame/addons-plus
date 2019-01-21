13d8a4e631d239c4d6b5f7512e4d387e66afa7ed
kalpana <kalpana@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-01-21 06:34:25 +0000

    OCA-OD12-957-Migration for Bank Reconciliation Report *Resolved difference amount shown as false in dashboard

--- skit_addons/skit_bank_reconcil/wizard/difference_amount.py
+++ skit_addons/skit_bank_reconcil/wizard/difference_amount.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, fields, _
+from odoo.tools.misc import formatLang
 
 
 class account_journal(models.Model):
     _inherit = 'account.journal'
         
-        
+     
+    @api.multi
+    def get_journal_dashboard_datas(self):
+        currency = self.currency_id or self.company_id.currency_id
+        val = super(account_journal, self).get_journal_dashboard_datas()
+        if val['difference'] is False:
+            val['difference'] = formatLang(self.env, 0, currency_obj=currency)
+        return val
+       
     @api.multi
     def print_report(self):
         
