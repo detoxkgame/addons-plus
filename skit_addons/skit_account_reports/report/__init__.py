6ee44b2d1102b7c629a41c8be8e09159f77d396a
dhanam <dhanam@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-01-23 05:41:44 +0000

    [OD12-1197] Journals Audit and Tax Report for OCA

--- skit_addons/skit_account_reports/report/__init__.py
+++ skit_addons/skit_account_reports/report/__init__.py
@@ -4,3 +4,5 @@ from . import account_balance
 from . import account_general_ledger
 from . import account_aged_partner_balance
 from . import account_partner_ledger
+from . import account_journal
+from . import account_tax
--- skit_addons/skit_account_reports/wizard/__init__.py
+++ skit_addons/skit_account_reports/wizard/__init__.py
@@ -6,3 +6,5 @@ from . import account_report_general_ledger
 from . import account_report_common_partner
 from . import account_report_aged_partner_balance
 from . import account_report_partner_ledger
+from . import account_report_print_journal
+from . import account_report_tax
