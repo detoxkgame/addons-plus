47ed9e03b7950cb871354ab4ecd7ed3fd042ca37
bharathi <bharathi@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-03-21 08:21:56 +0000

    [OD10-2084] - Pay Later - Invoice status can't change Paid OCA 11 - Super class called in inherit method

--- skit_addons/skit_pay_later/models/pos_session.py
+++ skit_addons/skit_pay_later/models/pos_session.py
@@ -17,45 +17,18 @@ class Skit_PosSession(models.Model):
     @api.multi
     def action_pos_session_close(self):
             # Close CashBox
-            for session in self:
-                company_id = session.config_id.company_id.id
-                ctx = dict(self.env.context, force_company=company_id,
-                           company_id=company_id)
-                for st in session.statement_ids:
-                    if abs(st.difference) > (
-                                st.journal_id.amount_authorized_diff):
-                        # The pos manager can close statements with maximums.
-                        if not self.env['ir.model.access'].check_groups(
-                                            "point_of_sale.group_pos_manager"):
-                            raise UserError(_("Your ending balance is too \
-                            different from the theoretical cash closing \
-                            (%.2f), the maximum allowed is: %.2f. \
-                            You can contact your manager to force it.") % (
-                                    st.difference,
-                                    (st.journal_id.amount_authorized_diff)))
-                    if (st.journal_id.type not in ['bank', 'cash']):
-                        raise UserError(_("The type of the journal for your \
-                        payment method should be bank or cash "))
-                    st.with_context(ctx).sudo().button_confirm_bank()
-            account_statement_line = self.env[
-                    'account.bank.statement.line'].search([
-                            ('statement_id', 'in', session.statement_ids.ids)])
-            self.with_context(ctx)._confirm_orders()
+            res = super(Skit_PosSession, self).action_pos_session_close()
+            session_id = self.id
+            account_statement_line = self.env['account.bank.statement.line'].search(
+                [('statement_id', 'in', self.statement_ids.ids)]
+            )
             for line in account_statement_line:
-                if line.pos_statement_id.session_id.id != session.id:
+                if line.pos_statement_id.session_id.id != session_id:
                     pay_later_order = self.env['pos.order'].search([
                             ('id', '=', int(line.pos_statement_id.id)),
-                            ('session_id', '!=', session.id)])
-                    self.with_context(ctx)._confirm_paylater_orders(
-                                                    pay_later_order)
-            self.write({'state': 'closed'})
-            return {
-                'type': 'ir.actions.client',
-                'name': 'Point of Sale Menu',
-                'tag': 'reload',
-                'params': {'menu_id': self.env.ref(
-                                'point_of_sale.menu_point_root').id},
-            }
+                            ('session_id', '!=', session_id)])
+                    self._confirm_paylater_orders(pay_later_order)
+            return res
 
     @api.model
     def create(self, values):
