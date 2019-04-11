731dfbe252e6ca6f304b1a122ab56c4eca4eab68
kalpana <kalpana@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-04-11 05:01:49 +0000

    [OD12-1496]-Odoo 12 xls report *print xls format *Added video link

--- skit_addons/skit_bank_reconcil/wizard/reconcil_report_wizard.py
+++ skit_addons/skit_bank_reconcil/wizard/reconcil_report_wizard.py
@@ -34,7 +34,23 @@ class skit_reconcil_report(models.TransientModel):
         result['date_to'] = 'date_to' in data['form'] and data['form']['date_to'] or False
         result['journal_id'] = 'journal_id' in data['form'] and data['form']['journal_id'] or False
         return result
-     
+    def _print_report(self, data):
+        records = self.env[data['model']].browse(data.get('ids', []))
+        return self.env.ref('skit_bank_reconcil.bank_reconciliation_report').report_action(records, data=data)
+ 
+    @api.multi
+    def check_report_xlsx(self):
+        self.ensure_one()
+        data = {}
+        data['ids'] = self.env.context.get('active_ids', [])
+        data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read(['date_from', 'date_to', 'journal_id'])[0]
+        used_context = self._build_contexts(data)
+        data['form']['used_context'] = dict(used_context, lang=self.env.context.get('lang', 'en_US'))
+        data['skit_report_type'] = "XLS"
+        data['skit_currency'] = self.env.user.currency_id.symbol
+        return self._print_report(data)
+      
     
 
 
