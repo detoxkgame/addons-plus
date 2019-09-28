391b4ec1dcc569f4884ca26842e8bec55842912a
e_selvi <e_selvi@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-09-28 09:56:09 +0000

    [TSA-2502]-1.Display "Upload file" button when Type = "document"            2. Display "Document URL" (existing column) button when Type = "video"            3. Rename channel as "Course"            4. Uploaded by (res.user) (new field)            5. AllowPreview - checkbox (new field)            6. Duration - time field - duration of video (new field)

--- skit_teachsa/skit_slide/models/slide_document.py
+++ skit_teachsa/skit_slide/models/slide_document.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields
 
 
 class Slide(models.Model):
     _inherit = 'slide.slide'
-
     slide_type = fields.Selection([
         ('quiz', 'Quiz'),
         ('presentation', 'Presentation'),
@@ -13,8 +11,7 @@ class Slide(models.Model):
         ('video', 'Video')],
         string='Type', required=True,
         default='document',
-        help="The document type will be set automatically based on the document URL and properties (e.g. height and width for presentation and document).")
-    channel_id = fields.Many2one('slide.channel', string="Course", required=True)
+        help="The document type will be set automatically based on the document URL and properties(e.g. height and width for presentation and document).")
     user_id = fields.Many2one('res.users', string="Uploaded by")
-    url = fields.Char('Document URL', help="Youtube or Google Document URL")
     completion_time = fields.Float("Duration")
+    is_preview = fields.Boolean(string="Allow preview")
