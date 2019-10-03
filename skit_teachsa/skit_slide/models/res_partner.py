34bdb39f15cc3dbff75bb2ab0c2e2c750f717a37
swathi <swathi@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-10-03 04:36:34 +0000

    [TSA-2519]-Add student tab customer(eLearning)-created tab student while checking parent checkbox and displayed student details in form

--- skit_teachsa/skit_slide/models/res_partner.py
+++ skit_teachsa/skit_slide/models/res_partner.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.tools.translate import html_translate
 
 class Partner(models.Model):
     _inherit = "res.partner"
+    
+    @api.onchange('student_id')
+    def _onchange_student_id(self):
+        if self.parent_id:
+            self.isstudent = "true"
 
     issponsor = fields.Boolean(string='IsSponsor', default=False)
     istutor = fields.Boolean(string='IsTutor', default=False)
+    isparent = fields.Boolean(string='IsParent', default=False)
+    isstudent = fields.Boolean(string='IsStudent', default=False)
     testimonial = fields.Html('Description', translate=html_translate, sanitize_attributes=False)
     website_publish = fields.Boolean( default=False)
+    parent_id = fields.Many2one('res.partner', string='Parent', domain=[('isparent', '=', True)])
+    student_id = fields.One2many('res.partner', 'parent_id',string='Children')
+                   
     website_published_state = fields.Selection([('published','Published'),
                                ('unpublished','Un Published')],
                                 string='Published state',default='unpublished')
