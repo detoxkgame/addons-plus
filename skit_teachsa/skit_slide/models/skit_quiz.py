09d17912c0e18cf7f032c60e8003f3054f489a8d
e_selvi <e_selvi@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-10-05 08:22:11 +0000

    [TSA-2529]-QUIZ MODULE
    1. CK editor for questions and answers
    -completed

--- skit_teachsa/skit_slide/models/skit_quiz.py
+++ skit_teachsa/skit_slide/models/skit_quiz.py
@@ -3,6 +3,8 @@ from odoo import api, models, fields
 from odoo.exceptions import UserError
 from datetime import datetime
 from odoo.addons import decimal_precision as dp
+from odoo.tools.translate import html_translate
+
 
 class SlideLearning(models.Model):
 
@@ -23,7 +25,8 @@ class SlideQuestion(models.Model):
     _name = 'slide.question'
 
     quiz_question_line_id = fields.Many2one('slide.slide', string='Quiz Question')
-    quiz_question = fields.Char(required=True)
+    # quiz_question = fields.Char(required=True)
+    quiz_question = fields.Html('Question Name', translate=html_translate, sanitize_attributes=False)
     quiz_answer_ids = fields.One2many('slide.answer', 'quiz_answer_line_id', string='Answers')
 
 
@@ -31,7 +34,8 @@ class SlideAnswer(models.Model):
     _name = 'slide.answer'
 
     quiz_answer_line_id = fields.Many2one('slide.question',string='Quiz Answer')
-    text_value = fields.Char(string='Answer',required=True)
+    # text_value = fields.Char(string='Answer',required=True)
+    text_value = fields.Html('Answer', translate=html_translate, sanitize_attributes=False)
     is_correct = fields.Boolean(string='Is Correct Answer')
 
 
