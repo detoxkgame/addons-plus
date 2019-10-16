4b7b635798f9cf031f7015612872aca0f5779bd5
mohanraj <mohanraj@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-10-16 11:43:48 +0000

    TSA-2551 Tech SA Website Design - Transparent Navigation Menu

---
+++ skit_teachsa/skit_teachsa_website/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Teach SA Website Design',
+    'summary': "Website Design",
+    'category': 'Website',
+    'author': 'Srikesh Infotech',
+    'company': 'Srikesh Infotech',
+    'website': 'http://www.srikeshinfotech.com',
+    'license': "AGPL-3",
+    'data': [
+        'views/assets.xml',
+        'views/index.xml',
+        ],
+    'depends': ['website'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
