f057cbecac2afa03571ead8af87a21bcac9d4299
dhanam <dhanam@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-10-19 04:13:28 +0000

    [OD12-2567] - Add Partner as filter in Partner Ledger report

--- skit_addons/skit_account_reports/__manifest__.py
+++ skit_addons/skit_account_reports/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Srikesh Infotech',
     'license': "AGPL-3",
     'website': 'http://www.srikeshinfotech.com',
-    'price': 20,
+    'price': 30,
     'currency': 'EUR',
     'description': """
         This module helps to generate Financial reports Balance Sheet, Profit and Loss, Trial Balance, General Ledger, Partner Ledger and Aged Partner Balance in PDF format
