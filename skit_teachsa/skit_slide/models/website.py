f967bc92bb0c9e3e797690c45520736e5329e6ed
mohanraj <mohanraj@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-10-21 04:50:01 +0000

    [TSA-2551]-Tech SA Website Design-added quotes of the day in webpage by SWATHI S

--- skit_teachsa/skit_slide/models/website.py
+++ skit_teachsa/skit_slide/models/website.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 from odoo.http import request
-
+import datetime
+from datetime import date
 class Website(models.Model):
 
     _inherit = "website"
@@ -14,3 +15,10 @@ class Website(models.Model):
         details = request.env['res.partner'].sudo().search([('testimonial','!=',None),('testimonial','!=',"<p><br></p>")])
         print (details)
         return details
+    
+    def calender_quotes(self):
+        val = date.today()
+        quotes = request.env['calendar.event'].sudo().search([('start_date','=',val)])
+        print (quotes)
+        print(val)
+        return quotes
