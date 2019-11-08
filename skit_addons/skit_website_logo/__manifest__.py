7bd5a9a1e516bef8b4becee925ce4224251ff9e8
mohanraj <mohanraj@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-11-08 11:43:34 +0000

    OD12-2497: Wet Market Website Logo Size Changed (Top Menu Transparent & Scroll Down the Page)

---
+++ skit_addons/skit_website_logo/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Website Logo',
+    'summary': "Website Logo Changes",
+    'category': 'Website',
+    'author': 'Srikesh Infotech',
+    'company': 'Srikesh Infotech',
+    'website': 'http://www.srikeshinfotech.com',
+    'license': "AGPL-3",
+    'data': [
+        'views/assets.xml',
+        'views/index.xml',
+        ],
+    'depends': ['website'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
