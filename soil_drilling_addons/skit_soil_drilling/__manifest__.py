0949df998c29f9d66ed424408632f02c0e757a8d
keerthana <keerthana@7d40962c-ea0b-6842-bdfc-460be783c1db>
2019-11-28 11:29:02 +0000

    [SD-2736] -Added soil drilling addon.

---
+++ soil_drilling_addons/skit_soil_drilling/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Skit Soil Drilling",
+
+    'summary': """
+        Soil Drilling
+        """,
+
+    'description': """
+       Soil Drilling
+    """,
+
+    'author': "Srikesh Infotech",
+    'website': "http://www.srikeshinfotech.com",
+    'category': 'Project',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': False,
+    'auto_install': False,
+    'application': True,
+}
